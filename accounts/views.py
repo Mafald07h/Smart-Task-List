f4fbaf4f80b12b770a2a8e69be3bde62eb7bb4de
Heitor <mafaldoheitor@gmail.com>
2026-01-12 15:14:33 -0300

    Criando a view de (login/sign-in) e deixando o formulário de login funcional

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.models import User
+from accounts.forms import LoginForm
 
 # Create your views here.
 def sign_in(request):
-    return render(request,"accounts/sign-in.html")
+    form = LoginForm(request.POST)
+    if request.method == "POST":
+        username = form["username"].value()
+        senha = form["senha"].value()
+
+        user = authenticate(
+            request,
+            username=username,
+            password=senha
+        )
+
+        if user is not None:
+            login(request,user)
+            messages.success(request,f"Bem vindo(a) {username}")
+            return redirect("dashboard")
+        else:
+            messages.error(request,"Usuário ou senha incorretos")
+            return redirect("login")
+    return render(request,"accounts/sign-in.html",{"form":form})
