7f8a7cbc291af3041fdfa19ea442f6f7cd411c23
Heitor <mafaldoheitor@gmail.com>
2026-01-23 08:59:50 -0300

    feat: Adicionando o models do app tasks e registrando no django-admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from tasks.models import Task
 
 # Register your models here.
+@admin.register(Task)
+class TasksAdmin(admin.ModelAdmin):
+    list_display = ("nome_task","data_inicio","data_fim","descricao")
+    search_fields = ("nome_task",)
+    list_filter = ("nome_task",)
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+class Task(models.Model):
+    user = models.ForeignKey(User,on_delete=models.PROTECT,related_name='tasks')
+    nome_task = models.CharField(max_length=100)
+    data_inicio = models.DateField()
+    data_fim = models.DateField()
+    descricao = models.TextField()
+
+    def __str__(self):
+        return f'{self.nome_task} - {self.descricao}'
+    
+    class Meta:
+        verbose_name = "Task"
+        verbose_name_plural = "Tasks"
+        ordering = ['nome_task']
