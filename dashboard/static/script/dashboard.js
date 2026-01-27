d276ece1e530e867616fe50e32a26939dfaa2164
Heitor <mafaldoheitor@gmail.com>
2026-01-27 19:52:26 -0300

    style: Adicionando estilo dark para cada item no dashboard

--- dashboard/static/script/dashboard.js
+++ dashboard/static/script/dashboard.js
@@ -8,13 +8,20 @@ const navbarDark = document.querySelector("[data-navbar]")
 const titleDark = document.querySelector("[data-title]")
 const subtextDark = document.querySelector("[data-subtext]")
 const dashboardLink = document.querySelectorAll("[data-dashboard-link]")
+const itemDashboard = document.querySelectorAll("[data-item]")
 
 btnChangeTheme.addEventListener("click", ()=> {
     bodyElement.classList.toggle("body-dark")
     navbarDark.classList.toggle("navbar__left-dark")
     titleDark.classList.toggle("title-dark")
+
     dashboardLink.forEach(link => {
         link.classList.toggle("dashboard__link-dark")
     })
+
     subtextDark.classList.toggle("subtext-dark")
+
+    itemDashboard.forEach(item => {
+        item.classList.toggle("item-dark")
+    })
 })
