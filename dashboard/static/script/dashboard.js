106c81556610b9154aea751d4846af63c4f05671
Heitor <mafaldoheitor@gmail.com>
2026-01-27 19:32:13 -0300

    style: Adicionando o estilo dark na mensagem abaixo a 'Dashboard'

--- dashboard/static/script/dashboard.js
+++ dashboard/static/script/dashboard.js
@@ -4,8 +4,11 @@ const tesksEfficiency = document.getElementById("efficiency")
 
 const btnChangeTheme = document.getElementById("changeTheme")
 const bodyElement = document.querySelector("[data-body]")
-
+const titleDark = document.querySelector("[data-title]")
+const subtextDark = document.querySelector("[data-subtext]")
 
 btnChangeTheme.addEventListener("click", ()=> {
     bodyElement.classList.toggle("body-dark")
+    titleDark.classList.toggle("title-dark")
+    subtextDark.classList.toggle("subtext-dark")
 })
