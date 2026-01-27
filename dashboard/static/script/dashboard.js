0fe52bdee282eefe234760ad32c9b59e335e5074
Heitor <mafaldoheitor@gmail.com>
2026-01-27 19:27:36 -0300

    style: Adicionando o estilo dark ao dashboard

--- dashboard/static/script/dashboard.js
+++ dashboard/static/script/dashboard.js
@@ -1,3 +1,11 @@
 const tasksSuccess = document.getElementById("tasks_completed")
 const tasksNotSuccess = document.getElementById("pendind_tasks")
 const tesksEfficiency = document.getElementById("efficiency")
+
+const btnChangeTheme = document.getElementById("changeTheme")
+const bodyElement = document.querySelector("[data-body]")
+
+
+btnChangeTheme.addEventListener("click", ()=> {
+    bodyElement.classList.toggle("body-dark")
+})
