42fda4ced5a646fadd6342a3bade98f9f8536705
Heitor <mafaldoheitor@gmail.com>
2026-01-27 19:39:48 -0300

    style: Adicionando o estilo dark a navbar esquerda

--- dashboard/static/script/dashboard.js
+++ dashboard/static/script/dashboard.js
@@ -4,11 +4,13 @@ const tesksEfficiency = document.getElementById("efficiency")
 
 const btnChangeTheme = document.getElementById("changeTheme")
 const bodyElement = document.querySelector("[data-body]")
+const navbarDark = document.querySelector("[data-navbar]")
 const titleDark = document.querySelector("[data-title]")
 const subtextDark = document.querySelector("[data-subtext]")
 
 btnChangeTheme.addEventListener("click", ()=> {
     bodyElement.classList.toggle("body-dark")
+    navbarDark.classList.toggle("navbar__left-dark")
     titleDark.classList.toggle("title-dark")
     subtextDark.classList.toggle("subtext-dark")
 })
