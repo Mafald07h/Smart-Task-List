f86d13a779d5db22c0df34504a444db125741c1a
Heitor <mafaldoheitor@gmail.com>
2026-01-28 17:02:39 -0300

    style: Adicionando o estilo dark para o titulo e para os links de navbar em tasks

--- tasks/static/script/tasks.js
+++ tasks/static/script/tasks.js
@@ -1,6 +1,18 @@
 const btnAddTask = document.getElementById("addTasks")
 const containerTasks = document.getElementById("container-tasks")
 
+const btnChangeTheme = document.getElementById("changeTheme")
+const titleDark = document.querySelector("[data-title]")
+const linkDark = document.querySelectorAll("[data-dashboard-link]")
+
+btnChangeTheme.addEventListener("click", ()=>{
+    titleDark.classList.toggle("title-dark")
+    linkDark.forEach(link, ()=>{
+        link.classList.toggle("dashboard__link-dark")
+    })
+})
+
 btnAddTask.addEventListener("click",()=>{
     containerTasks.classList.toggle("container-tasks-show")
 })
+
