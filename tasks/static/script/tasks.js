8298d067752cb1f851af6725c0fa79781737e78f
Heitor <mafaldoheitor@gmail.com>
2026-02-02 15:38:21 -0300

    refactor: Consertando o modo dark na parte de 'tasks'

--- tasks/static/script/tasks.js
+++ tasks/static/script/tasks.js
@@ -2,13 +2,13 @@ const btnAddTask = document.getElementById("addTasks")
 const containerTasks = document.getElementById("container-tasks")
 
 // Change Theme
-const btnChangeTheme = document.getElementById("changeTheme")
-const titleDark = document.querySelector("[data-title]")
+const btnChangeTheme2 = document.getElementById("changeTheme")
+const titleDark2 = document.querySelector("[data-title]")
 const linkDark = document.querySelectorAll("[data-dashboard-link]")
 const navbarLeftDark = document.querySelector("[data-navbar]")
 
-btnChangeTheme.addEventListener("click", ()=>{
-    titleDark.classList.toggle("title-dark")
+btnChangeTheme2.addEventListener("click", ()=>{
+    titleDark2.classList.toggle("title-dark")
     linkDark.forEach(link, ()=>{
         link.classList.toggle("dashboard__link-dark")
     })
